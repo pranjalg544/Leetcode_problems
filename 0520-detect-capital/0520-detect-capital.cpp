cbaa1cc86a1ba9dbd317966c751350e35bf6d8c5
pranjalg544 <pranjalg544@gmail.com>
2026-09-20 00:53:22 +0530

    Time: 0 ms (100%), Space: 8.1 MB (79.48%) - LeetHub-Neo

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        
+        int count = 0;
+        for(int i=0;i<word.size();i++){
+            if(isupper(word[i])){
+                count++;
+            }
+        }
+        if(count==word.size()) return true;
+        if(count==0) return true;
+        if(isupper(word[0]) && count == 1) return true;
+        else return false;
+    }
+};
